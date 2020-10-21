2e2cf571960a68607552510cab0ede7000aace27
Enzo <53284465+Enzo-MiMan@users.noreply.github.com>
2020-10-21 20:52:24 +0800

    update

---
+++ pcl_assoc/corres_gmapping_aided.py
@@ -0,0 +1,182 @@
+"""
+input:
+    1. timestamp matches between mm and gmapping
+    2. gmapping:  gmapping_T,  gmapping_R_matrix
+    3. LMR point cloud: overlay left, middle and right mm-wave point clouds
+
+parameter:
+    gap = 4   # read from config.yaml
+
+output:
+    source point cloud:  mm_src_GA_sample.txt
+    matched destination point cloud:  mm_dts_GA_sample.txt
+
+"""
+
+import open3d as o3d
+import numpy as np
+import re
+from os.path import join
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+import yaml
+import os
+from pcl_assoc.timestamp_match_mm_gmapping import timestamp_match
+from pcl_assoc.gmapping_R_T_from_csv import gmapping_TR
+
+
+
+
+
+
+def draw_matplotlib(src, dst):
+    fig = plt.figure(figsize=(20, 12))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(src[:, 0], src[:, 1], src[:, 2], s=10, c='r', marker='o')
+    ax.scatter(dst[:, 0], dst[:, 1], dst[:, 2], s=10, c='b', marker='o')
+    plt.show()
+
+
+def draw_o3d(src, dst):
+    pcd_src = o3d.geometry.PointCloud()
+    pcd_dst = o3d.geometry.PointCloud()
+
+    pcd_src.points = o3d.utility.Vector3dVector(src)
+    pcd_dst.points = o3d.utility.Vector3dVector(dst)
+
+    pcd_src.paint_uniform_color([1, 0.706, 0])
+    pcd_dst.paint_uniform_color([0, 0.651, 0.929])
+    o3d.visualization.draw_geometries([pcd_src, pcd_dst])
+
+
+
+def read_mm_pcl(mm_ts):
+
+    # read the mm-wave radar point clouds from two frames which have been matched with gmapping
+    mm_path = join(data_dir, sequence, 'mmwave_LMR_xyz', str(mm_ts) + '.xyz')
+    mm_collect = o3d.io.read_point_cloud(mm_path)
+    mm_collect = np.array(mm_collect.points)
+
+    return mm_collect
+
+
+def predict_next_pose(src_mm_ts, dst_mm_ts, src_gmap_ts, dst_gmap_ts, gmap_T, gmap_R):
+
+    mm_src_collect = read_mm_pcl(src_mm_ts)
+    mm_dst_collect = read_mm_pcl(dst_mm_ts)
+
+    # find the gap between two matched frames within gmapping timestamps
+    first_index = np.array(np.where(gmap_T[:, 0] == dst_gmap_ts))[0, 0]
+    second_index = np.array(np.where(gmap_T[:, 0] == src_gmap_ts))[0, 0]
+
+    # predict the pose of mm-wave destination point cloud
+    for k in range(second_index - first_index):
+        t = np.reshape(gmap_T[first_index + k, 1:4], (3, 1))
+        R = np.reshape(gmap_R[first_index + k, 1:10], (3, 3))
+
+        mm_src_pred = (np.matmul(R, mm_dst_collect.T) + t).T
+        mm_dst_collect = mm_src_pred
+
+    return mm_src_pred, mm_src_collect
+
+
+
+def nearest_neighbor(pc1, pc2):
+    '''
+    Find the nearest (Euclidean) neighbor in pc2 for each point in pc1
+    Input:
+        pc1: Nxm array of points
+        pc2: Kxm array of points
+    Output:
+        distances: Euclidean distances of the nearest neighbor
+        indices: pc2 indices of the nearest neighbor
+    '''
+
+    # assert src.shape == dst.shape
+
+    neigh = NearestNeighbors(n_neighbors=1)
+    neigh.fit(pc2)
+    distances, indices = neigh.kneighbors(pc1, return_distance=True)
+    return distances.ravel(), indices.ravel()
+
+
+def save_assoc_mmpcl(sample_src_indices, sample_dst_indices, src_mm_ts, dst_mm_ts):
+
+        assert len(sample_src_indices) == len(sample_dst_indices)
+
+        mm_src_collect = read_mm_pcl(src_mm_ts)
+        mm_dst_collect = read_mm_pcl(dst_mm_ts)
+
+        sample_src_collect = mm_src_collect[sample_src_indices, :]
+        sample_dst_collect = mm_dst_collect[sample_dst_indices, :]
+
+        # draw_matplotlib(sample_src_collect, sample_dst_collect)
+        # draw_o3d(mm_dst_calcul, mm_dst_collect)
+
+        l1 = ''
+        l2 = ''
+        for point in sample_src_collect:
+            l1 = (l1 + str(point[0]) + ' ' + str(point[1]) + ' ' + str(point[2]) + ' ')
+
+        for point in sample_dst_collect:
+            l2 = (l2 + str(point[0]) + ' ' + str(point[1]) + ' ' + str(point[2]) + ' ')
+
+        with open('/Users/manmi/Documents/data/square_data/mm_data/mm_src_gt_3d.txt', 'a+') as myfile:
+            myfile.write(str(src_mm_ts) + ' ' + l1 + '\n')
+
+        with open('/Users/manmi/Documents/data/square_data/mm_data/mm_dts_gt_3d.txt', 'a+') as myfile:
+            myfile.write(str(dst_mm_ts) + ' ' + l2 + '\n')
+
+
+if __name__ == '__main__':
+
+    # ------------------------ get config ------------------------
+
+    project_dir = os.path.dirname(os.getcwd())
+    with open(os.path.join(project_dir, 'config.yaml'), 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+    data_dir = cfg['base_conf']['data_base']
+    exp_names = cfg['radar']['exp_name']
+    sequence_names = cfg['radar']['all_sequences']
+    gap = cfg['radar']['gap']
+    DISTANCE_THRESHOLD = cfg['radar']['DISTANCE_THRESHOLD']
+
+
+    for sequence in exp_names:
+
+        ts_matches = timestamp_match(data_dir, sequence, gap)
+        gmap_T, gmap_R = gmapping_TR(data_dir, sequence)
+
+        num_match_pc = []
+
+        for i in range(1, len(ts_matches)):
+
+            src_mm_ts, src_gmap_ts = ts_matches[i, :]
+            dst_mm_ts, dst_gmap_ts = ts_matches[i-1, :]
+
+            """
+            compose R, t within consecutive gap frames from gmapping 
+            then apply R, t on source point cloud of mm-wave to predict the pose of point cloud in next frame
+            """
+            mm_dst_pred, mm_dst_collect = predict_next_pose(src_mm_ts, dst_mm_ts, src_gmap_ts, dst_gmap_ts, gmap_T, gmap_R)
+
+            # find the intersection between predicted point cloud and collected point cloud
+            distances, indices = nearest_neighbor(mm_dst_pred, mm_dst_collect)
+            pc_match = np.array([(i, v) for (i, v) in enumerate(indices)])
+
+            # intersection: sample the points by the DISTANCE_THRESHOLD between two frames of mm-wave point cloud
+            sample_src_indices = np.reshape(np.where(distances < DISTANCE_THRESHOLD), (-1))
+            sample_dst_indices = pc_match[sample_src_indices, 1]
+
+            assert len(sample_src_indices) >= 3
+
+            # record the number of intersection points for all point cloud pair correspondences
+            num_match_pc.append(len(sample_src_indices))
+
+            # save the intersection points separately (correspondence point cloud)
+            save_assoc_mmpcl(sample_src_indices, sample_dst_indices, src_mm_ts, dst_mm_ts)
+
+        num_match_pc = np.array(num_match_pc)
+        # print('DISTANCE_THRESHOLD =',DISTANCE_THRESHOLD, ', the frames of', np.where(num_match_pc < 3), 'points matching number are less than 3.')
+        # print(num_match_pc)
---
+++ pcl_assoc/mm_3_to_1.py
@@ -0,0 +1,184 @@
+"""
+input:
+
+
+parameter:
+
+
+output:
+
+
+"""
+
+
+import numpy as np
+import time
+import re
+import shutil
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-bright')
+import os
+import yaml
+from os.path import join
+import csv
+import collections
+# import cv2
+import math
+from pyquaternion import Quaternion
+from mmwave_bag import make_frames_from_csv
+
+# import pptk
+# import tqdm
+# from util.pcl2depth import velo_points_2_pano
+
+
+
+def quaternion_to_rotation_matrix(quat):
+    q = quat.copy()
+    n = np.dot(q, q)
+    if n < np.finfo(q.dtype).eps:
+        return np.identity(4)
+    q = q * np.sqrt(2.0 / n)
+    q = np.outer(q, q)
+    rot_matrix = np.array(
+        [[1.0 - q[2, 2] - q[3, 3], q[1, 2] + q[3, 0], q[1, 3] - q[2, 0], 0.0],
+         [q[1, 2] - q[3, 0], 1.0 - q[1, 1] - q[3, 3], q[2, 3] + q[1, 0], 0.0],
+         [q[1, 3] + q[2, 0], q[2, 3] - q[1, 0], 1.0 - q[1, 1] - q[2, 2], 0.0],
+         [0.0, 0.0, 0.0, 1.0]],
+        dtype=q.dtype)
+    return rot_matrix
+
+def euler_to_quaternion(yaw, pitch, roll):
+    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    return np.array([qx, qy, qz, qw])
+
+
+
+
+# get config
+project_dir = os.path.dirname(os.getcwd())
+with open(os.path.join(project_dir, 'config.yaml'), 'r') as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+
+data_dir = cfg['base_conf']['data_base']
+exp_names = cfg['radar']['exp_name']
+sequence_names = cfg['radar']['all_sequences']
+DISTANCE_THRESHOLD = cfg['radar']['DISTANCE_THRESHOLD']
+
+middle_transform = np.array(cfg['radar']['translation_matrix']['middle'])
+left_transform = np.array(cfg['radar']['translation_matrix']['left'])
+right_transform = np.array(cfg['radar']['translation_matrix']['right'])
+
+left_quaternion = Quaternion(axis=[0, 0, 1], angle=math.pi/2)
+right_quaternion = Quaternion(axis=[0, 0, 1], angle=-math.pi/2)
+
+
+# hyper-parameters
+align_interval = 5e7
+
+
+
+for sequence in exp_names:
+
+    # ------------------------ process middle ------------------------
+
+    csv_path = join(data_dir, str(sequence), '_slash_mmWaveDataHdl_slash_RScan_middle.csv')
+    if not os.path.exists(csv_path):
+        continue
+
+    readings_dict = make_frames_from_csv(csv_path)
+    data_dict = collections.OrderedDict(sorted(readings_dict.items()))  #!!! sort the dict before using
+
+    frames = list()
+    intensities = list()
+    timestamps = list()
+    valid_data = list()
+    count = 0
+
+    for timestamp, pts in data_dict.items():
+
+        # iterate each pt
+        heatmap_per_frame = list()
+        test_frame = list()
+        for pt in pts:
+            tmp = np.array(pt)
+            test_frame.append(tmp[[0, 1, 2, 3, 5]])
+            tmp_loc = tmp[0:3]
+
+            translated_tmp = tmp_loc + middle_transform[0:3]
+
+            tmp[0] = translated_tmp[0]
+            tmp[1] = translated_tmp[1]
+            tmp[2] = translated_tmp[2]
+            heatmap_per_frame.append(tmp[[0, 1, 2, 3, 5]])
+
+            # do not add empty frames
+            if not heatmap_per_frame:
+                continue
+
+        frames.append(np.array(heatmap_per_frame))
+        timestamps.append(timestamp)
+
+
+
+
+    # -------------- process left and right --------------------
+
+    for topic in ['_slash_mmWaveDataHdl_slash_RScan_left', '_slash_mmWaveDataHdl_slash_RScan_right']:
+
+        csv_path = join(data_dir, str(sequence), topic + '.csv')
+        if not os.path.exists(csv_path):
+            continue
+
+        readings_dict = make_frames_from_csv(csv_path)
+        data_dict = collections.OrderedDict(sorted(readings_dict.items()))  # !!! sort the dict before using
+
+        for timestamp, pts in data_dict.items():
+
+            # iterate each pt
+            heatmap_per_frame = list()
+            test_frame = list()
+            for pt in pts:
+                tmp = np.array(pt)
+                test_frame.append(tmp[[0, 1, 2, 3, 5]])
+                tmp_loc = tmp[0:3]
+
+                if topic == '_slash_mmWaveDataHdl_slash_RScan_left':
+                    translated_tmp = left_quaternion.rotate(tmp_loc) + left_transform[0:3]
+                elif topic == '_slash_mmWaveDataHdl_slash_RScan_right':
+                    translated_tmp = right_quaternion.rotate(tmp_loc) + right_transform[0:3]
+                assert (tmp[0] != translated_tmp[0])
+
+                tmp[0] = translated_tmp[0]
+                tmp[1] = translated_tmp[1]
+                tmp[2] = translated_tmp[2]
+                heatmap_per_frame.append(tmp[[0, 1, 2, 3, 5]])
+
+            # do not add empty frames
+            if not heatmap_per_frame:
+                continue
+
+            # overlay middle, left, and right
+            for i in range(0, len(timestamps)):
+                if abs(int(timestamp) - int(timestamps[i])) <= align_interval:
+                    frames[i] = np.concatenate((frames[i], np.array(heatmap_per_frame)))
+
+
+    file = os.path.join(data_dir, str(sequence), 'LMR_xyz')
+    if os.path.exists(file):
+        shutil.rmtree(file)
+        time.sleep(5)
+        os.makedirs(file)
+    else:
+        os.makedirs(file)
+
+    for i in range(len(timestamps)):
+        frame = frames[i]
+        for point in frame:
+            with open(os.path.join(data_dir, str(sequence), 'LMR_xyz', str(timestamps[i])+'.xyz'), 'a+') as file:
+                file.write(str(point[0]) + ' ' + str(point[1]) + ' ' + str(point[2]) + '\n')
+
+
